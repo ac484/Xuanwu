710e4e1461e8d3160f3af8d325e4a68dbaa85cb6
ac7x7x <ac7x7x@gmail.com>
2026-02-14 14:03:26 +0000

    1

---
+++ src/app/@header/(dashboard)/page.tsx
@@ -0,0 +1,5 @@
+import { Header } from '@/components/header'; // Please replace with your actual Header component
+
+export default function Page() {
+  return <Header />;
+}
---
+++ src/app/@sidebar/(dashboard)/page.tsx
@@ -0,0 +1,5 @@
+import { Sidebar } from '@/components/sidebar'; // Please replace with your actual Sidebar component
+
+export default function Page() {
+  return <Sidebar />;
+}
